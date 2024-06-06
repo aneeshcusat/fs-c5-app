95264ab11a716f946665abe0c7283420e96f0469
aneeshkumar <aneeshcusat@gmail.com>
2024-06-06 14:16:07 -0700

    2.5.0.17

--- fui/service-worker.js
+++ fui/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.288d62ff927cbad9e4a7dd1b7788bbeb.js"
+  "/precache-manifest.3a31889771d5a780ee1841a4354a4007.js"
 );
 
 self.addEventListener('message', (event) => {
